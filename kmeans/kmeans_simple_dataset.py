a11b860f83783661a54ec1041fa6297ce292ae9f
Jakub Jędrych <kubajedrych100@gmail.com>
2025-06-04 12:11:09 +0200

    Create toy dataset and save KMeans clustering image

--- kmeans/kmeans_simple_dataset.py
+++ kmeans/kmeans_simple_dataset.py
@@ -3,11 +3,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.datasets import make_circles
+from sklearn.cluster import kmeans_plusplus,KMeans
+
+
 
 def make_circle():
-    data = make_circles(1000)
+    data = make_circles(1000,noise=0.05)
     return data
 
+def plot_circles(X,y):
+    palette = sns.dark_palette((20, 60, 50), input="husl")
+    sns.set_style("darkgrid", {"grid.color": ".1", "grid.linestyle": ":"})
+    sns.scatterplot(x = X[:,0],y = X[:,1],hue= y,palette=palette)
+    plt.xlabel("X1")
+    plt.ylabel("X2")
+    plt.legend(["0","1"])
+    plt.title("Circles for KMeans")
+    plt.tight_layout()
+    plt.xticks([])
+    plt.yticks([])
+    ax = plt.gca()
+    ax.set_facecolor("#AFAFAF")
+    plt.savefig("kmeans_comparison_images/circle_true_labels.svg")
+    plt.show()
 
-X,y = make_circle()
+   
+
+if __name__ == "__main__":
+    X,y = make_circle()
+    plot_circles(X,y)
 
